80c99c04b4b72385402ab3c0808a5911d121ae4d
JakubTomaszStasiak <261177@edu.p.lodz.pl>
2026-05-17 13:30:06 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,28 +4,64 @@ using namespace std;
 
 class Vehicle {
     public:
-        float mass;
-        int topSpeed;
-        string color;
-        bool isLicenseRequired;
+    	float mass;
+	    int topSpeed;
+	    string color;
+	    bool isLicenseRequired;
+	    
+	    virtual void fuelType(){
+	        cout << "Fuel depends on the specific vehicle" << endl;
+	    }
+	    
 };
 
-class Car: public Vehicle{
+class Car: public Vehicle {
     public:
-        bool hasSteeringOnLeft;
-        bool isManual;
+    	bool hasSteeringOnLeft;
+    	bool isManual;
+    	string fuel;
+    	
+    	void fuelType(){
+    	    cout << fuel << endl;
+    	}
+    	
+    	Car(){
+    	    fuel = "Pb98";
+    	}
+    	
+    	Car(string x){
+    	    fuel = x;
+    	}
 };
 
-class 
+class Bike: public Vehicle {
+    public:
+        float frameType;
+        bool hasBasket;
+        
+        void fuelType(){
+            cout << "muscles" << endl;
+        }
+
+};
 
 
 int main()
 {
-    Car polonez;
+    Vehicle **vehicles = new Vehicle*[5];
     
-    polonez.mass = 30;
+    vehicles[0] = new Car;
+    vehicles[1] = new Car("diesel");
+    vehicles[2] = new Vehicle;
+    vehicles[3] = new Bike;
+    vehicles[4] = new Car("Pb95");
     
-    cout << polonez.mass;
+    vehicles[0]->fuelType();
+    vehicles[1]->fuelType();
+    vehicles[2]->fuelType();
+    vehicles[3]->fuelType();
+    vehicles[4]->fuelType();
+    
 
-    return 0;
+	return 0;
 }
